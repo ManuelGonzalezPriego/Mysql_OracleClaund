103e80cc506bcc45458bbfa9a14cd2304f20a021
Manuel González Prigo <manuelgonzalezpriego1@gmail.com>
2024-11-05 17:09:09 +0100

    Actualizar base de datos

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.39, for Linux (x86_64)
 --
--- Host: localhost    Database: Manuel
+-- Host: localhost    Database: Manolo
 -- ------------------------------------------------------
 -- Server version	8.0.39-0ubuntu0.24.04.2
 
@@ -14,6 +14,33 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(12) DEFAULT NULL,
+  `telf` int DEFAULT NULL,
+  `saldo` double(20,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nombre` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'Manolo',687343434,12000.50),(2,'Pepe',677445484,1500.50),(3,'Juan',911568909,100500.99);
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `entradas`
@@ -37,8 +64,64 @@ CREATE TABLE `entradas` (
 
 LOCK TABLES `entradas` WRITE;
 /*!40000 ALTER TABLE `entradas` DISABLE KEYS */;
-INSERT INTO `entradas` VALUES (1,'2024-10-21 00:00:00'),(1,'2024-10-22 00:00:00'),(1,'2024-10-22 02:58:20'),(1,'2024-10-22 03:06:29'),(1,'2024-10-22 03:08:13'),(5,'2024-10-22 02:32:04'),(6,'2024-10-22 02:36:46'),(7,'2024-10-22 15:28:32'),(7,'2024-10-22 15:51:30'),(8,'2024-10-22 15:30:08'),(8,'2024-10-22 15:31:26'),(8,'2024-10-22 15:36:13'),(8,'2024-10-22 15:36:22'),(8,'2024-10-22 15:50:46'),(8,'2024-10-22 15:51:24'),(9,'2024-10-22 15:48:47'),(10,'2024-10-24 22:50:27');
+INSERT INTO `entradas` VALUES (12,'2024-11-04 17:18:45'),(13,'2024-11-04 17:27:18');
 /*!40000 ALTER TABLE `entradas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ordenes`
+--
+
+DROP TABLE IF EXISTS `ordenes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ordenes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_Cliente` int DEFAULT NULL,
+  `asunto` varchar(50) DEFAULT NULL,
+  `descripcion` varchar(500) DEFAULT NULL,
+  `fechaApertura` timestamp NULL DEFAULT NULL,
+  `fechaCierre` timestamp NULL DEFAULT NULL,
+  `importe` double(20,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_cliente` (`id_Cliente`),
+  CONSTRAINT `fk_cliente` FOREIGN KEY (`id_Cliente`) REFERENCES `clientes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ordenes`
+--
+
+LOCK TABLES `ordenes` WRITE;
+/*!40000 ALTER TABLE `ordenes` DISABLE KEYS */;
+INSERT INTO `ordenes` VALUES (1,2,'Chacho dame mi dinero','Chacho los 500 yaaaaaaaa','2024-10-29 16:29:06',NULL,-500.10),(2,2,'Prestamo','No tengo un dirp','2024-01-02 00:00:00','2024-10-29 17:03:40',1000.00);
+/*!40000 ALTER TABLE `ordenes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tickets`
+--
+
+DROP TABLE IF EXISTS `tickets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tickets` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `pelicula` varchar(16) DEFAULT NULL,
+  `cantidad` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tickets`
+--
+
+LOCK TABLES `tickets` WRITE;
+/*!40000 ALTER TABLE `tickets` DISABLE KEYS */;
+INSERT INTO `tickets` VALUES (1,'Tu y yo',0),(2,'Mariano y Carmen',20),(3,'Sweetie Car',20);
+/*!40000 ALTER TABLE `tickets` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -51,11 +134,11 @@ DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE `usuarios` (
   `idUsuario` int NOT NULL AUTO_INCREMENT,
   `nombreLogin` varchar(12) DEFAULT NULL,
-  `contrasena` varchar(32) DEFAULT NULL,
+  `contrasena` varchar(64) DEFAULT NULL,
   `nombreCompleto` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`idUsuario`),
   UNIQUE KEY `nombreLogin` (`nombreLogin`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,9 +147,30 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'Manuel','10400c6faf166902b52fb97042f1e0eb','Manuel Ejemplo'),(4,'Magomez','12cdb9b24211557ef1802bf5a875fd2c','Manuel Gómez'),(5,'hola','4d186321c1a7f0f354b297e8914ab240','hola'),(6,'Finish','d79695776a5b40f7cadbee1f91a85c82','ultimo'),(7,'Manuel','c4ca4238a0b923820dcc509a6f75849b','Manuel Gonzalez 2'),(8,'1','c4ca4238a0b923820dcc509a6f75849b','1'),(9,'2','C81E728D9D4C2F636F067F89CC14862C','Manuel'),(10,'pepe','C4CA4238A0B923820DCC509A6F75849B','pepe');
+INSERT INTO `usuarios` VALUES (11,'prueva','8cf31238b4a396f90185f770921dc63b31f4d48604026ffd4f8ddc80','pdroasdfasdfasdf'),(12,'Manolo','15a62c0c679a2e55c919607832b69f31495284ab203beebf4a1adec0','Manolo González'),(13,'pepe','a7470858e79c282bc2f6adfd831b132672dfd1224c1e78cbf5bcd057','pepe');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `SHA2_Trigger` BEFORE INSERT ON `usuarios` FOR EACH ROW BEGIN
+    SET NEW.contrasena = SHA2(NEW.contrasena, 224);
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+
+--
+-- Dumping routines for database 'Manolo'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -77,4 +181,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-24 20:57:33
+-- Dump completed on 2024-11-04 16:46:27
